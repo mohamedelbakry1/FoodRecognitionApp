68b913a3c614e7b999e8fb5085d9d2d824fad2b0
Mohamed Elbakary <albqrym58@gmail.com>
2026-01-22 11:31:50 +0200

    Implement Global Error Handling Middlewares with Validation Errors

---
+++ FoodRecognitionApp.Shared/ErrorModels/ErrorDetails.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodRecognitionApp.Shared.ErrorModels
+{
+    public class ErrorDetails
+    {
+        public int StatusCode { get; set; }
+        public string ErrorMessage { get; set; } = null!;
+    }
+}
---
+++ FoodRecognitionApp.Web/Middlewares/GlobalErrorHandlingMiddlewares.cs
@@ -0,0 +1,53 @@
+﻿using FoodRecognitionApp.Domain.Exceptions.BadRequest;
+using FoodRecognitionApp.Domain.Exceptions.NotFound;
+using FoodRecognitionApp.Domain.Exceptions.UnAuthorized;
+using FoodRecognitionApp.Shared.ErrorModels;
+
+namespace FoodRecognitionApp.Web.Middlewares
+{
+    public class GlobalErrorHandlingMiddlewares(RequestDelegate _next)
+    {
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next.Invoke(context);
+
+                if(context.Response.StatusCode == StatusCodes.Status404NotFound)
+                {
+                    context.Response.ContentType = "application/json";
+
+                    var response = new ErrorDetails()
+                    {
+                        StatusCode = context.Response.StatusCode,
+                        ErrorMessage = $"End Point {context.Request.Path} was Not Found !!"
+                    };
+
+                    await context.Response.WriteAsJsonAsync(response);
+                }
+            }
+            catch(Exception ex)
+            {
+                context.Response.StatusCode = ex switch
+                {
+                    NotFoundException => StatusCodes.Status404NotFound,
+                    BadRequestException => StatusCodes.Status400BadRequest,
+                    UnAuthorizedException => StatusCodes.Status401Unauthorized,
+                    _ => StatusCodes.Status500InternalServerError
+                };
+
+                context.Response.ContentType = "application/json";
+
+
+                var response = new ErrorDetails()
+                {
+                    StatusCode = context.Response.StatusCode,
+                    ErrorMessage = ex.Message
+                };
+
+                await context.Response.WriteAsJsonAsync(response);
+
+            }
+        }
+    }
+}
