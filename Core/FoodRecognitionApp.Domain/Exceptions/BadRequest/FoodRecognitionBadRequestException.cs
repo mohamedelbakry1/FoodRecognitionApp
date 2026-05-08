ab635807a361f89ba85a5516efe881b09afdeca2
Mohamed Elbakary <albqrym58@gmail.com>
2026-05-08 10:33:28 +0300

    Integrate with new Ai Model wiht high Accuracy and portion size estimation

---
+++ Core/FoodRecognitionApp.Domain/Exceptions/BadRequest/FoodRecognitionBadRequestException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodRecognitionApp.Domain.Exceptions.BadRequest
+{
+    public class FoodRecognitionBadRequestException() : BadRequestException("Failed to Detect Food")
+    {
+    }
+}
